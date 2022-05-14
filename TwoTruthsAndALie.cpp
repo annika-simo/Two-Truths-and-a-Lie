a34232e985e221866610ed816447c8c41cc26763
Annika Leida Simovart <annikasimovart@gmail.com>
2022-05-14 11:04:19 -0400

    Add files via upload

---
+++ TwoTruthsAndALie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+ *	This program outputs three statements: two truths 
+ *	and one lie. If you look in the comments of this code
+ *	you can see which is which.
+ */
+
+int main() {
+	std::cout << "I have a black belt." << std::endl; //truth
+	std::cout << "I've been to europe.\n"; //truth
+	std::cout << "I'm fluent in Spanish and Vietnamese.\n"; //lie
+
+	return 0;
+}
